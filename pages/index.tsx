a2f66b5e2d9a893de7c2c5a24ca659abbe4cdbf7
Michael Snegurski <mishasneg@gmail.com>
2022-08-07 16:31:33 +0200

    Try page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 import { Page } from '@saas-ui/pro'
 
 const Home: NextPage = () => {
-  return <Page />
+    return (
+        <Page title="Page Title">
+            Page Body
+        </Page>
+    )
 }
 
 export default Home
